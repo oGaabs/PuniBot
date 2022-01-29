adf1143764556b9b8a24b8944cd5ffbfccf63db8
Gabriel Santana <gabriel04.ok@gmail.com>
2022-01-29 15:22:33 -0300

    🔧 |  Fix in help handler

--- listeners/message.js
+++ listeners/message.js
@@ -4,10 +4,11 @@ module.exports = async function onMessage(client) {
     const prefix = client.prefix
     const commands = client.commands
     client.on('messageCreate', async message => {
+        if (message.author.bot) return
         if (message.mentions.has(client.user)) commands.get('help').execute(message, '', client)
         if(!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return
         const args = message.content.slice(prefix.length).split(/ +/);
-        args.shift() 
+        args.shift()
         const command = args[0].toLowerCase()
         if ((initCommands('commands', command)))
             commands.get(command).execute(message, args, client)
@@ -18,13 +19,11 @@ module.exports = async function onMessage(client) {
         Fs.readdirSync(path).forEach(file =>{
             try{
                 let filePath = path + '/' + file
-                if (file.endsWith('.js') && file.replace('.js','') === command){
+                if (file.endsWith('.js') && file.replace('.js','') === command)
                     result = true
-                }
                 if (Fs.lstatSync(filePath).isDirectory()){
-                    if (initCommands(filePath, command)) {
+                    if (initCommands(filePath, command))
                         result = true
-                    }
                 }
             }
             catch (err){
