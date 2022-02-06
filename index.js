c41d7db29721e9ef82c428a06334c31b8151cad2
Gabriel Santana <gabriel04.ok@gmail.com>
2022-02-05 23:59:12 -0300

    🌐 | New breakout/offline prevent system

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 require("dotenv").config()
-const express = require('express')
 
-const app = express()
-const porta = process.env.PORT || 3000
-
-app.get('/', async (_req, res) => {
-    res.send('<h1> Servidor de internet NodeJS funcionando.</h1>')
-})
-app.listen(porta, () => console.log('> Servidor NodeJS funcionando na porta ' + porta + '.'))
+const startHost = require('./host')
+startHost()
 
 const { Intents } = require('discord.js')
 const PuniBot = require('./PuniBot')
 const client = new PuniBot({ intents: new Intents(32767) })
 
 client.once('ready', async () => {
-    client.prefix = process.env.PREFIX.toLowerCase()
     client.guild = client.guilds.cache.get('926539282733203546')
     client.canais = client.channels.cache
     client.footer = { text: client.guild.name, iconURL: client.guild.iconURL() }
     client.initListeners('./listeners')
     client.initCommands('./commands')
-    console.log(`Memory: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100 } MB`)
     client.users.fetch('407734609967841299', false).then((owner) => {
         owner.createDM().then(dmChannel => {
             dmChannel.messages.fetch({ limit: 100 })
@@ -41,22 +33,24 @@ client.once('ready', async () => {
             })
         })
     })
+    console.log(`Memory: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100 } MB`)
 })
 
 process.on('unhandledRejection', error => {
     console.log('Erro : ', error)
     console.log('\nLogando novamente...')
     client.destroy()
-    client.login(process.env.TOKEN).then(() =>{
-        console.log(`\nLogado como ${client.user.tag}`+
-                    `\nMemory: ${Math.round(process.memoryUsage().rss / 1024 / 1024 * 100) / 100 } MB\n`)
-    })
+    botLogin()
 })
 
-client.login(process.env.TOKEN)
-.then(() => {
-    console.log(`\nLogado como ${client.user.tag}.`)
-})
-.catch(err => {
-    console.log(`Falha ao iniciar o bot : ${err}`)
-})
+function botLogin() {
+    client.login(process.env.TOKEN)
+    .then(() => {
+        console.log(`\nLogado como ${client.user.tag}.`)
+    })
+    .catch(err => {
+        console.log(`Falha ao iniciar o bot : ${err}`)
+    })
+}
+
+botLogin()
