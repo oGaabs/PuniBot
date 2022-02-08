85d4508f0ac1be033fa50676fac08eeec8d8c7c4
Gabriel Santana <gabriel04.ok@gmail.com>
2022-02-07 22:11:57 -0300

    🎨 | Command redesign (GIF)

--- utils/gifSearch.js
+++ utils/gifSearch.js
@@ -4,7 +4,7 @@ module.exports = {
     getGif: async function (searchTerm) {
         const response = await nodeFetch(`https://g.tenor.com/v1/search?q=${searchTerm}&key=${process.env.TENORKEY}&ContentFilter=high`)
             .then(res => res.json())
-        const gifPost = response.results[Math.floor(Math.random() * json.results.length)]
+        const gifPost = response.results[Math.floor(Math.random() * response.results.length)]
         return { image: gifPost.media[0].gif.url, url: gifPost.url }
     }
 }
