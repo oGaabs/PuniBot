106edc46174b7bda13357ae61762db1ffbbc1a2e
Gabriel Santana <gabriel04.ok@gmail.com>
2022-03-30 23:31:45 -0300

    🎨 | Command redesign (Invite)

--- commands/information/invite.js
+++ commands/information/invite.js
@@ -22,13 +22,13 @@ module.exports = {
         const inviteChannel = message.mentions.channels.first() ?? (channels.filter(channel => channel.isText()).first() || guild.systemChannel)
 
         if (!channels.find(channel => channel === inviteChannel) ||
-            !inviteChannel.permissionsFor(user).has("CREATE_INSTANT_INVITE"))
+            !inviteChannel.permissionsFor(user).has('CREATE_INSTANT_INVITE'))
             return message.channel.send({ embeds: [permissionErrorEmbed] })
 
         const invite = await inviteChannel.createInvite({ unique: true, reason: 'Requisitado pelo ' + message.author.tag })
         const inviteEmbed = new MessageEmbed()
             .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-            .setTitle(" **Puni Invite** ")
+            .setTitle(' **Puni Invite** ')
             .setColor(client.getColor('default'))
             .setDescription(`Quer me convidar para seu servidor? Entre por aqui ${invite}`)
             .setFooter(client.getFooter(message))
