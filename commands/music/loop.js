6d8098f7d0cf678e20d584d861b1656877bee5c8
Gabriel Santana <gabriel04.ok@gmail.com>
2022-04-03 04:09:06 -0300

    🎵 | New command (Loop)

---
+++ commands/music/loop.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'loop',
+    aliases: ['autoplay','semparar', 'dontstop'],
+    description: 'Loop na playlist',
+    execute: async (message, _args, client) => {
+        const { queue } = message.client
+        if (!queue) return message.reply('Não ha nenhuma musica sendo tocada!')
+
+        const serverQueue = queue.get(message.guild.id)
+        if (!serverQueue) return message.reply('Não ha nenhuma musica sendo tocada!')
+
+        const status = !serverQueue.autoPlay
+        serverQueue.autoPlay = status
+
+        const autoplayEmbed = new MessageEmbed()
+            .setColor(client.getColor('default'))
+            .setTitle(`🎵 | AutoPlay: ${status ? 'ON' : 'OFF'}`)
+        message.channel.send({ embeds: [autoplayEmbed] })
+    }
+}
