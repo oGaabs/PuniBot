cb9c39e1016f6a7b0abdfefb456f9a93edecfc26
Gabriel Santana <gabriel04.ok@gmail.com>
2022-04-03 04:08:51 -0300

    🎵 | New command (Queue)

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,8 +1,10 @@
+const { MessageEmbed } = require('discord.js')
+
 module.exports = {
     name: 'queue',
-    alises: ['playlist', 'song', 'songs', 'music', 'musics', 'tocando'],
+    aliases: ['playlist', 'songs', 'lista', 'list', 'musicas', 'musics'],
     description: 'Músicas na playlist',
-    execute: async (message, _args, _client) => {
+    execute: async (message, _args, client) => {
         const { queue } = message.client
         if (!queue) return message.reply('Não ha nenhuma musica sendo tocada!')
 
@@ -10,13 +12,18 @@ module.exports = {
         if (!serverQueue) return message.reply('Não ha nenhuma musica sendo tocada!')
         const songs = serverQueue.songs
         const currentlySong = songs[0]
-        message.reply(`Tocando **${currentlySong.title}**\nLink: ${currentlySong.url}`)
 
-        let playlist = ''
-        for (const song of songs) {
-            const { title, url } = song
-            playlist = playlist.concat((`Música: \`${title}\`\nLink: **${url}**\n`))
+        let playlist = []
+        for (let { title, url }  of songs) {
+            url = 'https://' + currentlySong.url
+            playlist.push(`**[${title.substring(0, 51)}](${url})**\n`)
         }
-        message.channel.send(playlist).then(msg => msg.suppressEmbeds(true))
+        const listEmbed = new MessageEmbed()
+            .setColor(client.getColor('default'))
+            .setTitle('Lista de Reprodução')
+            .setThumbnail(currentlySong.thumbnail)
+            .setDescription(`Now playing: **[${currentlySong.title.substring(0, 51)}](${currentlySong.url})**\n\n`+
+                              playlist.join(' '))
+        message.channel.send({ embeds: [listEmbed] })
     }
 }
