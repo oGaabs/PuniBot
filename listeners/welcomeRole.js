7d393ad00a74a3e81352d4f89734092087a2ceaa
Gabriel Silva <gabriel04.ok@gmail.com>
2022-06-19 06:16:06 -0300

    🛠 | Rename module (Autorole => welcomeRole)

---
+++ listeners/welcomeRole.js
@@ -0,0 +1,13 @@
+module.exports = async function autoRole(client) {
+    const guild = client.guilds.cache.get('926539282733203546')
+    const separadorRole = guild.roles.cache.get('926709141014196265')
+    const viajanteRole = guild.roles.cache.get('930038268173643808')
+    const botRole = guild.roles.cache.get('926567305155055717')
+    setInterval(() => {
+        const members = guild.members.cache.filter(m => !m.user.bot)
+        members.forEach(member => member.roles.add([separadorRole, viajanteRole]))
+
+        const bots = guild.members.cache.filter(m => m.user.bot)
+        bots.forEach(member => member.roles.add(botRole))
+    }, 25000)
+}
