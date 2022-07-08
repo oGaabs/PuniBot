fa11a4b348b38d6013b2becbb7a9c1c6a1c77728
Gabriel Silva <gabriel04.ok@gmail.com>
2022-07-07 23:33:59 -0300

    🎨 | Command redesign (Update to setPresence)

--- listeners/botActivity.js
+++ listeners/botActivity.js
@@ -5,28 +5,25 @@ module.exports = async function botActivity(client) {
             prefix: botPrefix } = client
 
     const stats = ['online', 'dnd', 'idle']
-    const statsLength = stats.length
-    setInterval(() => {
-        puniBot.setStatus(stats[Math.floor(Math.random() * statsLength)])
-    }, 20000)
 
-
-    const status = [
-        { name: '🎥 Cineminha!', type: 'STREAMING', url: 'https://www.netflix.com/watch/81073022?trackId=14170033&tctx=1%2C0%2Cbb356764-ae2a-42ea-afac-69e403b2ac9e-42496442%2C09551ab6-8494-4e9b-bdca-5f41cf065a47_24951814X9XX1641901381014%2C09551ab6-8494-4e9b-bdca-5f41cf065a47_ROOT%2C%2C%2C' },
-        { name: '🍮 Como fazer um pudim?', type: 'PLAYING' },
-        { name: `${guild.memberCount} membros 🥳`, type: 'PLAYING' },
-        { name: `${botTag} ✔️`, type: 'PLAYING' },
-        { name: `${botPrefix} help`, type: 'PLAYING' },
-        { name: 'Sem minha crush 💔', type: 'PLAYING' },
-        { name: '🏆 Anda perdido ? me mencione!', type: 'PLAYING' },
-        { name: '🔑 Entre em contato para reportar qualquer bug.', type: 'PLAYING' },
-        { name: '🍮 Pudim na lua?', type: 'PLAYING' },
-        { name: '🍮 Desfrute de um belo pudim', type: 'PLAYING' },
-        { name: '🍮 Pudim Pudim Pudim', type: 'PLAYING' },
-        { name: '👩‍🚀 Mais Comandos legais para Você!', type: 'PLAYING' }
-    ]
-    const statusLength = status.length
     setInterval(() => {
-        puniBot.setActivity(status[Math.floor(Math.random() * statusLength)])
+        const activities = [
+            { name: '🎥 Cineminha!', type: 'STREAMING', url: 'https://www.netflix.com/watch/81073022?trackId=14170033&tctx=1%2C0%2Cbb356764-ae2a-42ea-afac-69e403b2ac9e-42496442%2C09551ab6-8494-4e9b-bdca-5f41cf065a47_24951814X9XX1641901381014%2C09551ab6-8494-4e9b-bdca-5f41cf065a47_ROOT%2C%2C%2C' },
+            { name: '🍮 Como fazer um pudim?', type: 'PLAYING' },
+            { name: `${guild.memberCount} membros 🥳`, type: 'PLAYING' },
+            { name: `${botTag} ✔️`, type: 'PLAYING' },
+            { name: `${botPrefix} help`, type: 'PLAYING' },
+            { name: 'Sem minha crush 💔', type: 'PLAYING' },
+            { name: '🏆 Anda perdido ? me mencione!', type: 'PLAYING' },
+            { name: '🔑 Entre em contato para reportar qualquer bug.', type: 'PLAYING' },
+            { name: '🍮 Pudim na lua?', type: 'PLAYING' },
+            { name: '🍮 Desfrute de um belo pudim', type: 'PLAYING' },
+            { name: '🍮 Pudim Pudim Pudim', type: 'PLAYING' },
+            { name: '👩‍🚀 Mais Comandos legais para Você!', type: 'PLAYING' }
+        ]
+        const randomActivity = activities[Math.floor(Math.random() * activities.length)]
+        const randomStatus = stats[Math.floor(Math.random() * stats.length)]
+
+        puniBot.setPresence({ activities: [randomActivity], status: randomStatus })
     }, 20000)
 }
