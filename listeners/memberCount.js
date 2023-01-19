2973ecea9dbeea881b49028fdd88c34ea70be57e
Gabriel Santana <gabriel04.ok@gmail.com>
2023-01-19 05:58:39 -0300

    🗳️ | New project structure

--- listeners/memberCount.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function memberCount(client) {
-    const guild = client.guilds.cache.get('926539282733203546')
-    const channel = client.channels.cache.get('927167938706931752')
-    setInterval(() => {
-        const memberCount = guild.memberCount
-        channel.setName(`👥 Membros: ${memberCount}`)
-    }, 10000)
-}
--- utils/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    colors: require('./colors'),
-    defaultEmbed: require('./defaultEmbed'),
-    formatter: require('./formatersDiscord'),
-    gifSearch: require('./gifSearch'),
-    logger: require('./logger'),
-}
