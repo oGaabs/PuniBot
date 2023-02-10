2c44d835a16647ed70cef8275472688e167a43e7
Gabriel Santana <gabriel04.ok@gmail.com>
2023-02-10 05:17:35 -0300

    🎨 | Command redesign gifSearch (update nodefetch to axios)

--- src/utils/gifSearch.js
+++ src/utils/gifSearch.js
@@ -1,15 +1,22 @@
-const nodeFetch = require('node-fetch')
+// Considerando os Termos de Serviços do Discord, utilizamos um filtro do TenorAPI para encontrar os gifs,
+// que está conforme a PG-13: Pais fortemente alertados, alguns materiais podem ser impróprios
+// para crianças menores de 13 anos, em que 13 é a idade minima para usar o Discord no Brasil.
+
+const axios = require('axios')
 
 module.exports = {
     getGif: async function (searchTerm) {
-        // Considerando os Termos de Serviços do Discord, utilizamos um filtro do TenorAPI para encontrar os gifs,
-        // que está conforme a PG-13: Pais fortemente alertados, alguns materiais podem ser impróprios
-        // para crianças menores de 13 anos, em que 13 é a idade minima para usar o Discord no Brasil.
-        const response = await nodeFetch(`https://g.tenor.com/v1/search?q=${searchTerm}&key=${process.env.TENORKEY}&contentfilter=low&media_filter=minimal`)
-            .then(res => res.json())
-        if(response.results.length < 1)
+        const response = await axios.get(`https://g.tenor.com/v1/search?q=${searchTerm}&key=${process.env.TENORKEY}&contentfilter=low&media_filter=minimal`)
+            .then(response => {
+                return response.data
+            })
+            .catch(err => console.error(err))
+
+        if (!response || response.results.length < 1)
             return null
+
         const gifPost = response.results[Math.floor(Math.random() * response.results.length)]
         return { image: gifPost.media[0].gif.url, url: gifPost.url }
     }
 }
+
