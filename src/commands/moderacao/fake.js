b42e49b0fb07890f1e1473b82ba1c65a172162c6
Gabriel Santana <gabriel04.ok@gmail.com>
2023-02-10 05:14:33 -0300

    🎨 | Command redesign Fake Message (v14 & Refactor)

--- src/commands/moderacao/fake.js
+++ src/commands/moderacao/fake.js
@@ -1,17 +1,19 @@
 const Command = require('../../utils/base/Command.js')
 
+const { EmbedBuilder, PermissionsBitField: { Flags: PERMISSIONS } } = require('discord.js')
+
 class Fake extends Command {
     constructor(client) {
         super(client, {
             name: 'fake',
-            aliases: ['fakear', 'troll'],
+            aliases: ['fakear', 'troll', 'messagebot'],
             description: 'Fakear um usuario e mensagem!',
             category: 'moderação',
             args: '(Usuario) (Canal) (Mensagem) '
         })
     }
 
-    async execute (message, args, client, isUsedByOtherCommand){
+    async execute(message, args, client, isUsedByOtherCommand) {
         const permissionErrorEmbed = await client.defaultEmbed.getPermissionError(
             '*Verifique se você ou o bot possui a permissão:*',
             '`ADMINISTRATOR`'
@@ -20,21 +22,47 @@ class Fake extends Command {
         permissionErrorEmbed.setColor(client.colors['default'])
         permissionErrorEmbed.setFooter(client.getFooter(message.guild))
 
-        if (!isUsedByOtherCommand && !message.member.permissions.has('ADMINISTRATOR'))
+        if (!isUsedByOtherCommand && !message.member.permissions.has(PERMISSIONS.Administrator))
             return message.channel.send({ embeds: [permissionErrorEmbed] })
 
         const userToFake = message.mentions.users.first()
-        if (!userToFake) return message.channel.send('Você precisa mencionar um usuário!')
+        if (!userToFake) return message.channel.send(`Você precisa mencionar um usuário! ${client.prefix} fake ${this.args}`)
+
+        const messageToFake = args.slice(2).join(' ')
+        if (messageToFake.length < 1) return message.channel.send(`Você precisa digitar uma mensagem! ${client.prefix} fake ${this.args}`)
 
         const channelToSend = message.mentions.channels.first() ?? message.channel
 
-        const messageToFake = args.slice(2).join(' ')
-        if (messageToFake.length < 1) return message.channel.send('Você precisa digitar uma mensagem!')
+        const fakeData = {
+            name: userToFake.username,
+            avatar: userToFake.displayAvatarURL({ format: 'png' }),
+            channel: channelToSend,
+            message: messageToFake
+        }
 
-        channelToSend.createWebhook(userToFake.username, { avatar: userToFake.displayAvatarURL({ format: 'png' }) }).then(async webhook => {
-            webhook.send(messageToFake)
-            message.delete()
-        })
+        if (message.content.startsWith(client.prefix + ' messageBot')) {
+            this.sendEmbedMessage(fakeData, channelToSend)
+            return message.delete()
+        }
+
+        this.createWebhook(fakeData, channelToSend)
+            .then(async webhook => {
+                webhook.send(fakeData.message)
+                message.delete()
+            })
+    }
+
+    createWebhook(fakeData, channelToSend) {
+        return channelToSend.createWebhook({ name: fakeData.name, avatar: fakeData.avatar })
+    }
+
+    async sendEmbedMessage(fakeData, channelToSend) {
+        const botEmbed = new EmbedBuilder()
+            .setDescription(`**${fakeData.message}**`)
+
+        this.createWebhook(fakeData, channelToSend).then(async webhook => webhook.send({
+            embeds: [botEmbed],
+        }))
     }
 }
 
