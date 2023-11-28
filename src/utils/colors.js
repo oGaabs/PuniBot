f7435b005b2169ca6865dd0e307aa31e9707727f
oGaabs <gabriel04.ok@gmail.com>
2023-11-28 04:08:44 -0300

    🚀 | feat: add new color

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,7 +1,11 @@
 module.exports = {
     default: '#9B59B6',
+    noColor: '#2f3136',
     alert: '#EFFF00',
     black: '#000000',
     green: '#B3FFB3',
-    log: '#2F3136'
+    log: '#2F3136',
+    randomColor: () => {
+        return Math.floor(Math.random() * 16777215).toString(16)
+    }
 }
